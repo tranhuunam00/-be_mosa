6944fac1ef996f90ac684521e28984892f143f15
namTh <tranhuunam23022000@gmail.com>
2023-10-27 23:54:18 +0700

    u

--- src/controllers/sensor.js
+++ src/controllers/sensor.js
@@ -3,6 +3,8 @@ const httpResponses = require('../utils/httpResponses');
 const sensorService = require('../services/sensor');
 const xlsx = require('node-xlsx');
 const constants = require('../constants/constants');
+const fs = require('fs');
+const path = require('path');
 
 const createSensors = async (req, res) => {
   try {
@@ -30,49 +32,48 @@ const createSensors = async (req, res) => {
 
 const exportFileSensorData = async (req, res) => {
   try {
-    const sensors = await sensorService.getAllSensors();
-    console.log(sensors[100]);
-    const data = [constants.HEADER_ACCELEROMETER_EXPORT];
-    sensors.forEach((item) => {
-      const [x, y, z] = item.value.split('%');
-
-      data.push([0, 0, item.time, x, y, z]);
+    const sensors = await sensorService.getAllSensors({
+      customer: req.query.customer,
     });
 
-    var buffer = xlsx.build([{ name: 'mySheetName', data: data }]);
+    let csv = constants.HEADER_ACCELEROMETER_EXPORT;
 
-    const fileName = 'accelerometer.csv';
+    sensors.forEach((item) => {
+      const [x, y, z] = item.value.split('%');
+      // Append data rows to the CSV string
+      csv += `\n${item.customer},0,${item.time},${x},${y},${z}`;
+    });
+
+    const fileName = 'accelerometer.csv'; // Set the file name with a .csv extension
 
     res.writeHead(200, {
       'Content-Disposition': `attachment; filename="${fileName}"`,
-      'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      'Content-Type': 'text/csv', // Set the content type to CSV
     });
 
-    const download = Buffer.from(buffer, 'base64');
-
-    res.end(download);
+    // Send the CSV string as the response
+    res.end(csv);
   } catch (e) {
     logger.error(e.message);
-    return res.internalServer(e.message);
+    return res.status(500).send(e.message); // Assuming `internalServer` is a custom response function, change it to `res.status(500).send(e.message)`
   }
 };
 
 const exportFileSensorDataTxt = async (req, res) => {
   try {
-    const sensors = await sensorService.getAllSensors();
-    console.log(sensors[100]);
-    const data = [constants.HEADER_ACCELEROMETER_EXPORT];
+    const sensors = await sensorService.getAllSensors({
+      customer: req.query.customer,
+    });
 
+    fs.unlinkSync('data.txt');
     sensors.forEach((item, index) => {
       let lable = 1;
-      if (index >= 990 && index < 1980) lable = 2;
-      if (index >= 1980 && index < 2970) lable = 3;
-      if (index >= 2970) lable = 4;
-
       const [x, y, z] = item.value.split('%');
-      data.push(`0,${lable},${item.time.getTime()},${x},${y},${z};`);
+      fs.appendFileSync('data.txt', `${item.customer},${lable},${item.time},${x},${y},${z};\n`);
     });
-    return res.json(data);
+    // return res.json(data);
+    const filePath = path.resolve('data.txt');
+    return res.sendFile(filePath);
   } catch (e) {
     logger.error(e.message);
     return res.internalServer(e.message);
